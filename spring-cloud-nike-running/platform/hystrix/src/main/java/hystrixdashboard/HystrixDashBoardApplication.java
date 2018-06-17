da71601b838d65798087978d682fb8cef9dc58a0
XihuanZeng <zengx272@gmail.com>
2018-06-17 00:00:30 -0400

    add starting files for Hystrix

---
+++ spring-cloud-nike-running/platform/hystrix/src/main/java/hystrixdashboard/HystrixDashBoardApplication.java
@@ -0,0 +1,13 @@
+package hystrixdashboard;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by xihuan on 18-6-16.
+ */
+
+@SpringBootApplication
+public class HystrixDashBoardApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
